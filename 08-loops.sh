b739581438a15b6eb6dee1abb6b2b6da7a33e43c
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-10 13:56:47 +0530

    # commit

--- 08-loops.sh
+++ 08-loops.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for laptopname in macbook dell hp lenovo ;
-echo laptopname = $laptopname
-sleep 1
+for laptop in macbook dell hp lenovo ;
+   echo laptopname  = $laptopname
+   sleep 1
 done
